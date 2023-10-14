df330bbfabb92cf04a69897eb370ddd58c4c060c
JOY-JB <joybarua0058@gmail.com>
2023-10-14 11:41:01 +0600

    add auth to create admin route

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { userController } from './user.controller';
 import { UserValidation } from './user.validation';
@@ -18,6 +20,7 @@ router.post(
 router.post(
   '/create-admin',
   validateRequest(UserValidation.UserValidationSchema),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN),
   userController.createAdmin
 );
 
